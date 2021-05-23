65b9b3fe6880e190955f4c0dfbe4860093cea649
Olga-Smolianinova <olga.smol2015@gmail.com>
2021-05-23 20:07:24 +0300

    delete comments

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const { Command } = require("commander"); //импортируется пакет commander для удобного парсинга аргументов командной строки.
+const { Command } = require("commander");
 
-// Data
 const {
   listContacts,
   getContactById,
@@ -17,12 +16,10 @@ program
   .option("-e, --email <type>", "user email")
   .option("-p, --phone <type>", "user phone");
 
-// чтобы достать распарсенные данные
 program.parse(process.argv);
 
 const argv = program.opts(); //{}
 
-// Используй готовую функцию invokeAction() которая получает тип выполняемого действия и необходимые аргументы. Функция вызывает соответствующий метод из файла contacts.js передавая ему необходимые аргументы.
 function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
